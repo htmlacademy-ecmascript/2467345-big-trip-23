da40512fd3d01a5fc3e722ddff2496e6ac9f4a5a
Alexandr <61061837+AlexandrVanchukov@users.noreply.github.com>
2024-06-13 14:50:10 +0300

    module8-task2

--- src/presenter/new-point-presenter.js
+++ src/presenter/new-point-presenter.js
@@ -1,6 +1,5 @@
 import { remove, render, RenderPosition } from '../framework/render.js';
 import FormEditView from '../view/form-edit.js';
-import { nanoid } from 'nanoid';
 import { UserAction, UpdateType } from '../const.js';
 
 export default class NewPointPresenter {
@@ -61,15 +60,31 @@ export default class NewPointPresenter {
     document.removeEventListener('keydown', this.#escKeyDownHandler);
   }
 
+  setSaving() {
+    this.#pointEditComponent.updateElement({
+      isDisabled: true,
+      isSaving: true,
+    });
+  }
+
+  setAborting() {
+    const resetFormState = () => {
+      this.#pointEditComponent.updateElement({
+        isDisabled: false,
+        isSaving: false,
+        isDeleting: false,
+      });
+    };
+
+    this.#pointEditComponent.shake(resetFormState);
+  }
+
   #handleFormSubmit = (point) => {
     this.#handleDataChange(
       UserAction.ADD_POINT,
       UpdateType.MINOR,
-      // Пока у нас нет сервера, который бы после сохранения
-      // выдывал честный id задачи, нам нужно позаботиться об этом самим
-      { id: nanoid(), ...point }
+      point
     );
-    this.destroy();
   };
 
   #handleDeleteClick = () => {
